f7051694f5e1ff82c2895340c99292c3de142789
faxt <jebeltran59@mise.edu.co>
2025-10-13 20:21:05 -0500

    cambios de la pagina web

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+  //cargar menu
   fetch('menu.html')
     .then(response => response.text())
     .then(data => {
@@ -6,12 +7,42 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .catch(error => console.error('Error cargando menú:', error));
 
+  //cargar footer
+  fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('footer-container').innerHTML = data;
+    })
+    .catch(error => console.error('Error cargando pie de página:', error));
 
-fetch('footer.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('footer-container').innerHTML = data;
-  })
-  .catch(error => console.error('Error cargando pie de página:', error));   
+  // Manejar descarga de archivos
+  const botonDescarga = document.getElementById('btn-descargar');
+  if (botonDescarga) {
+    botonDescarga.addEventListener('click', () => {
+      const archivos = [
+        {
+          url: 'documentos/Hoja_de_vida_Jelver_Beltran.docx_2025_1.pdf'
+        },
+        {
+          url: 'documentos/Hoja_de_vida_Jelver_Beltran.docx_2025_2.docx'
+        }
+      ];
 
-  });
+      archivos.forEach((archivo, index) => {
+        setTimeout(() => {
+          const enlace = document.createElement('a');
+          enlace.href = archivo.url;
+          // Extraer el nombre automáticamente desde la URL
+          enlace.download = archivo.url.split('/').pop();
+          document.body.appendChild(enlace);
+          
+
+          console.log(`Iniciando descarga de: ${archivo.url}`);
+          enlace.click();
+          document.body.removeChild(enlace);
+        }, index * 1000);
+
+      });
+    });
+  }
+});
